92e77a65c923eb3045d3f7923fbbe4511f9785a6
noorisarosh <noorisarosh@gmail.com>
2025-11-09 23:41:10 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.159af20350f4409da4bd64ed094e2563',
+  appName: 'YorkUFind',
+  webDir: 'dist',
+  server: {
+    url: 'https://159af203-50f4-409d-a4bd-64ed094e2563.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  }
+};
+
+export default config;
